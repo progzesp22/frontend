2814d6131c9ff7d75470329c89a1c1a6864bd206
Krzysztof Nowak <krzysztof9nowak@gmail.com>
2022-11-06 14:58:05 +0100

    Added user type selection and started working on task list

--- MyApplication/app/src/main/java/com/example/myapplication/ListTasksFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ListTasksFragment.java
@@ -9,17 +9,23 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
+import com.example.myapplication.databinding.FragmentListTasksBinding;
 import com.example.myapplication.databinding.FragmentSelectUserTypeBinding;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SelectUserTypeFragment#newInstance} factory method to
+ * Use the {@link ListTasksFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SelectUserTypeFragment extends Fragment {
-    private FragmentSelectUserTypeBinding binding;
+public class ListTasksFragment extends Fragment {
+    FragmentListTasksBinding binding;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,7 +36,7 @@ public class SelectUserTypeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public SelectUserTypeFragment() {
+    public ListTasksFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +46,11 @@ public class SelectUserTypeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SelectUserTypeFragment.
+     * @return A new instance of fragment ListTasksFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SelectUserTypeFragment newInstance(String param1, String param2) {
-        SelectUserTypeFragment fragment = new SelectUserTypeFragment();
+    public static ListTasksFragment newInstance(String param1, String param2) {
+        ListTasksFragment fragment = new ListTasksFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,30 +70,29 @@ public class SelectUserTypeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentSelectUserTypeBinding.inflate(inflater, container, false);
+        // Inflate the layout for this fragment
+        binding = FragmentListTasksBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.usertypeRadio.setOnCheckedChangeListener((group, checkedId) -> {
-            RadioButton checkedRadioButton = (RadioButton)group.findViewById(checkedId);
-            boolean gm = checkedRadioButton == binding.gmRadioButton;
+        List<Task> tasks = new LinkedList<>();
+        tasks.add(new Task(0, "Szyszunie", "Zbierz 20 świeżych szyszek"));
+        tasks.add(new Task(1, "Problem Collatza", "Rozstrzygnij problem Collatza"));
 
-            if(gm){
-                binding.gmModeRadio.setVisibility(View.VISIBLE);
-            } else{
-                binding.gmModeRadio.setVisibility(View.INVISIBLE);
-            }
-        });
+        displayTasks(tasks);
+    }
 
-        binding.startButton.setOnClickListener(view1 -> {
-            int checkedRadioButtonId = binding.usertypeRadio.getCheckedRadioButtonId();
-            if(checkedRadioButtonId == R.id.playerRadioButton){
-                NavHostFragment.findNavController(this).navigate(R.id.action_playerSelected);
-            }
+    private void displayTasks(List<Task> tasks){
+        binding.tasksLayout.removeAllViews();
 
-        });
+        for(Task task : tasks){
+//            Button test = new Button(this.getContext());
+            TextView test = new TextView(getContext());
+            test.setText(task.getName());
+            binding.tasksLayout.addView(test);
+        }
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
 
 import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -19,6 +19,8 @@ import com.example.myapplication.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,9 +42,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
 
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
     }
@@ -75,4 +74,6 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
 }
