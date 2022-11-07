1bc86ca0f0133a91b2bc14f366e03d8c4baf9bf6
ksurdyk1 <karsur121@wp.pl>
2022-11-07 17:02:59 +0100

    added GM screens with task checking

---
+++ MyApplication/app/src/main/java/com/example/myapplication/GMTaskCheckFragment.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.databinding.FragmentGmTaskCheckBinding;
+
+
+public class GMTaskCheckFragment extends Fragment {
+    private FragmentGmTaskCheckBinding binding;
+
+
+    public GMTaskCheckFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentGmTaskCheckBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TasksModel model = new ViewModelProvider(requireActivity()).get(TasksModel.class);
+        Task task = model.getActiveTask();
+
+        if(task == null) return;
+        binding.titleText.setText(task.getName());
+        binding.descriptionText.setText(task.getDescription());
+    }
+}
