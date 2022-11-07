9384c7ced056608485b02345d6a3be6b6d94da8d
ksurdyk1 <karsur121@wp.pl>
2022-11-07 15:07:17 +0100

    added option for adding tasks

---
+++ MyApplication/app/src/main/java/com/example/myapplication/GMTaskAddFragment.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.databinding.FragmentGmTaskAddBinding;
+
+
+public class GMTaskAddFragment extends Fragment {
+    private FragmentGmTaskAddBinding binding;
+
+
+    public GMTaskAddFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentGmTaskAddBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+}
