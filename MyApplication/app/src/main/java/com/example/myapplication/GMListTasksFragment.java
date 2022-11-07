65a606a4d4d7f14c127ef54a349b82ef3b44224c
ksurdyk1 <karsur121@wp.pl>
2022-11-07 15:07:17 +0100

    added option for adding tasks

--- MyApplication/app/src/main/java/com/example/myapplication/GMListTasksFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/GMListTasksFragment.java
@@ -10,13 +10,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.myapplication.databinding.FragmentListTasksBinding;
+import com.example.myapplication.databinding.FragmentGmListTasksBinding;
 
 import java.util.List;
 
 
 public class GMListTasksFragment extends Fragment {
-    FragmentListTasksBinding binding;
+    FragmentGmListTasksBinding binding;
 
     public GMListTasksFragment() {
         // Required empty public constructor
@@ -27,7 +27,7 @@ public class GMListTasksFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentListTasksBinding.inflate(inflater, container, false);
+        binding = FragmentGmListTasksBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
@@ -37,7 +37,8 @@ public class GMListTasksFragment extends Fragment {
         TasksModel model = new ViewModelProvider(requireActivity()).get(TasksModel.class);
         model.getTasks().observe(requireActivity(), this::displayTasks);
 
-       binding.refreshTasks.setOnClickListener(view1 -> model.refreshTasks());
+        binding.addTask.setOnClickListener(view1 ->
+                NavHostFragment.findNavController(this).navigate(R.id.action_addTask) );
     }
 
     private void displayTasks(List<Task> tasks){
