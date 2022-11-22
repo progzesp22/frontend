069c7864c855dbc05b69f8730818378c68f234fb
Xgreg2001 <83090501+Xgreg2001@users.noreply.github.com>
2022-11-22 08:38:22 +0100

    comment explaining MyCaptureActivity existence

--- MyApplication/app/src/main/java/com/example/myapplication/MyCaptureActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MyCaptureActivity.java
@@ -2,5 +2,6 @@ package com.example.myapplication;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+// This class exist to force portratit orientation via Manifest.xml
 public class MyCaptureActivity extends CaptureActivity {
 }
