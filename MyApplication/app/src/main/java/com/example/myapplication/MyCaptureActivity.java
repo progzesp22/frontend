5302f4e0332509270afbbe23a5f69dbb762a0338
Marek Świergoń <91944872+Swmar23@users.noreply.github.com>
2022-11-22 08:39:36 +0100

    Skanowanie kodów QR (proof of concept) (#14)
    * Do ekranu TaskAnswerFragment dodano przycisk umożliwiający uruchomienie metody skanującej kod QR. Do skanowania kodów wykorzystano bibliotekę xzing.
    
    * comment explaining MyCaptureActivity existence
    
    Co-authored-by: Xgreg2001 <83090501+Xgreg2001@users.noreply.github.com>

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MyCaptureActivity.java
@@ -0,0 +1,7 @@
+package com.example.myapplication;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+// This class exist to force portratit orientation via Manifest.xml
+public class MyCaptureActivity extends CaptureActivity {
+}
