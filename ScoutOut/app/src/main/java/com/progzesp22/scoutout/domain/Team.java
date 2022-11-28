d08b6db70364a7e5ca11773b044d006384b181bd
Xgreg2001 <83090501+Xgreg2001@users.noreply.github.com>
2022-11-28 21:06:07 +0100

    RequestHandler update to be compatible with proto 1.0.6 (#36)

---
+++ ScoutOut/app/src/main/java/com/progzesp22/scoutout/domain/Team.java
@@ -0,0 +1,70 @@
+package com.progzesp22.scoutout.domain;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public class Team {
+
+    private long id;
+    private long gameId;
+    private String name;
+    private String creator;
+    private List<String> members;
+
+    public Team(long id, long gameId, String name, String creator, List<String> members) {
+        this.id = id;
+        this.gameId = gameId;
+        this.name = name;
+        this.creator = creator;
+        this.members = members;
+    }
+
+    public static Team fromJson(JSONObject json) {
+        return new Team(json.optLong("id"),
+                json.optLong("gameId"),
+                json.optString("name"),
+                json.optString("creator"),
+                null); // TODO: parse members
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(long gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public List<String> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<String> members) {
+        this.members = members;
+    }
+}
