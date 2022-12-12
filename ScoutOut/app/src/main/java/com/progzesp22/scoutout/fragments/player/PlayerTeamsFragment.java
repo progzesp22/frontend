3f798b913e77f8ad1c5c6fd11fee9bd16ea84b5d
ksurdyk1 <karsur121@wp.pl>
2022-12-12 18:10:29 +0100

    added toast info & made small visual changes

--- ScoutOut/app/src/main/java/com/progzesp22/scoutout/fragments/player/PlayerTeamsFragment.java
+++ ScoutOut/app/src/main/java/com/progzesp22/scoutout/fragments/player/PlayerTeamsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -37,7 +38,9 @@ public class PlayerTeamsFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        binding.createTeamButton.setOnClickListener(view1 -> {
+            binding.newTeamName.setText("");
+            Toast.makeText(view1.getContext(), "Team created successfully", Toast.LENGTH_SHORT).show();});
         binding.recycler.setLayoutManager(new LinearLayoutManager(getContext()));
         TeamsModel model = new ViewModelProvider(requireActivity()).get(TeamsModel.class);
         model.getTeams().observe(getViewLifecycleOwner(), this::displayTeams);
