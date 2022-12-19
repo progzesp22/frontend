2e6423e19b7224a46530c8c0fdd7f330ad3b4d28
ksurdyk1 <karsur121@wp.pl>
2022-12-19 16:25:13 +0100

    added request handler

--- ScoutOut/app/src/main/java/com/progzesp22/scoutout/fragments/player/PlayerTeamsFragment.java
+++ ScoutOut/app/src/main/java/com/progzesp22/scoutout/fragments/player/PlayerTeamsFragment.java
@@ -1,6 +1,7 @@
 package com.progzesp22.scoutout.fragments.player;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,21 +14,28 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.progzesp22.scoutout.MainActivity;
 import com.progzesp22.scoutout.R;
 import com.progzesp22.scoutout.databinding.FragmentPlayerTeamsBinding;
+import com.progzesp22.scoutout.domain.Entity;
 import com.progzesp22.scoutout.domain.GamesModel;
 import com.progzesp22.scoutout.domain.Team;
 import com.progzesp22.scoutout.domain.TeamsModel;
+import com.progzesp22.scoutout.domain.UserModel;
 import com.progzesp22.scoutout.fragments.TeamsAdapter;
+
+import org.json.JSONException;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 
 public class PlayerTeamsFragment extends Fragment {
     FragmentPlayerTeamsBinding binding;
+    NavController navController;
 
     public PlayerTeamsFragment() {
         // Required empty public constructor
@@ -42,19 +50,32 @@ public class PlayerTeamsFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.createTeamButton.setOnClickListener(view1 -> {
-            binding.newTeamName.setText("");
-            Toast.makeText(view1.getContext(), "Team created successfully", Toast.LENGTH_SHORT).show();});
         binding.recycler.setLayoutManager(new LinearLayoutManager(getContext()));
         TeamsModel model = new ViewModelProvider(requireActivity()).get(TeamsModel.class);
         model.getTeams().observe(getViewLifecycleOwner(), this::displayTeams);
         model.refresh();
         GamesModel gamesModel = new ViewModelProvider(requireActivity()).get(GamesModel.class);
-        binding.gameNameText.setText(gamesModel.getActiveGame().getName());
-        binding.gameMasterText.setText(gamesModel.getActiveGame().getGameMaster());
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'\n'HH:mm", Locale.getDefault());
-        binding.startTimeText.setText(dateFormat.format(gamesModel.getActiveGame().getStartTime()));
-        binding.endTimeText.setText(dateFormat.format(gamesModel.getActiveGame().getEndTime()));
+        UserModel userModel = new ViewModelProvider(requireActivity()).get(UserModel.class);
+        setGameInfoTexts(gamesModel);
+        binding.createTeamButton.setOnClickListener(view1 -> {
+            List<String> members = new ArrayList<>();
+            members.add(userModel.getUsername());
+            Team newTeam = new Team(
+                    Entity.UNKNOWN_ID,
+                    gamesModel.getActiveGame().getId(),
+                    String.valueOf(binding.newTeamName.getText()),
+                    userModel.getUsername(),
+                    members
+            );
+            MainActivity.requestHandler.postTeams(newTeam, response -> {
+                Toast.makeText(view1.getContext(), "Team created successfully", Toast.LENGTH_SHORT).show();
+            }, error -> {
+                Toast.makeText(view1.getContext(), "Error, team not created", Toast.LENGTH_SHORT).show();
+            } );
+            binding.newTeamName.setText("");
+            model.setActiveTeam(newTeam);
+            NavHostFragment.findNavController(this).navigate(R.id.action_playerTeamsFragment_to_userGamesFragment);
+            });
 
     }
 
@@ -62,6 +83,14 @@ public class PlayerTeamsFragment extends Fragment {
         TeamsModel model = new ViewModelProvider(requireActivity()).get(TeamsModel.class);
         NavController navController = NavHostFragment.findNavController(this);
         binding.recycler.setAdapter(new TeamsAdapter(teams, navController, model));
+    }
+
+    private void setGameInfoTexts(GamesModel gamesModel) {
+        binding.gameNameText.setText(gamesModel.getActiveGame().getName());
+        binding.gameMasterText.setText(gamesModel.getActiveGame().getGameMaster());
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'\n'HH:mm", Locale.getDefault());
+        binding.startTimeText.setText(dateFormat.format(gamesModel.getActiveGame().getStartTime()));
+        binding.endTimeText.setText(dateFormat.format(gamesModel.getActiveGame().getEndTime()));
     }
 
 
